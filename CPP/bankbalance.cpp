bdc1215f06eb46b5da96b3fe4ef4860523eaafc3
Some-T <jamie.cropley@gmail.com>
2016-04-16 19:25:19 +0100

    Cash Machine Simulation
    Cash Machine Simulation written in C++

---
+++ CPP/bankbalance.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+ 
+
+using namespace std;
+
+
+int main() {
+
+    float withdraw;
+    string currency = "£";
+    float balance = 792.86;
+
+    cout << "Your current balance is: " << currency <<  balance << endl;
+    cout << "Enter the amount you would like to withdraw: ";
+    cout << currency;
+    cin >> withdraw;
+    
+    if (balance < withdraw) {
+        cout << "INSUFFICENT FUNDS" << endl;
+    } else {
+        cout << "EJECTING CASH" << endl;
+    }
+
+    balance = balance - withdraw;
+
+    cout << "Your new balance is: " << currency << balance << endl;
+
+    
+system("pause");
+}
