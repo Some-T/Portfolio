39fe2bef0c7c74b7eff260d4097d606720ee82eb
Some-T <jamie.cropley@gmail.com>
2016-07-02 16:54:26 +0100

    Update
    Update

---
+++ SWIFT/Count-Characters/Count-Characters/Count-Characters/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Count-Characters
+//
+//  Created by Jamie Cropley on 02/07/2016.
+//  Copyright © 2016 Jamie Cropley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
