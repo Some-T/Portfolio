23c6311137929573ddcdf75f945cbc08ccb045f0
Some-T <jamie.cropley@gmail.com>
2016-07-11 20:15:10 +0100

    Update
    Update

--- HASKELL/MyPlayground/MyPlayground.hsproj/MyPlayground.hs
+++ HASKELL/MyPlayground/MyPlayground.hsproj/MyPlayground.hs
@@ -9,4 +9,57 @@ inc x = x + 1
 
 f(x) = x + 1
 
-    
+
+
+
+average a b  = (a + b) / 2.0
+
+
+double x = 2 * x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+square x = x * x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+showResult show = "Testing" ++ 123
+
+
+
+
+showAreaOfCircle show r = pi*r^2
