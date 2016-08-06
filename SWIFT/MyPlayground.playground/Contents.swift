184a8c11ed9469f08038f49d44c02750ba26cb87
Some-T <jamie.cropley@gmail.com>
2016-08-07 00:00:42 +0100

    Update
    Update

---
+++ SWIFT/MyPlayground.playground/Contents.swift
@@ -0,0 +1,86 @@
+/*
+
+let startPassengers = 20
+var totalPassengers = startPassengers
+
+var nextPickup = 5
+var totalCars = 5
+
+//train takes on five passengers.
+print("train will pickup \(nextPickup) passengers")
+print("train will have \(totalPassengers + nextPickup) passengers")
+
+
+totalPassengers += nextPickup
+print("picked up \(nextPickup) passengers.")
+print ("Train has \(totalPassengers) passengers now")
+
+//train will drop off 3 passengers
+nextPickup = 3
+print("train will drop off \nextPickup) passengers")
+print("train will have \totalPassengers - nextPickup)")
+
+totalPassengers -= nextPickup
+print("dropped off \(nextPickup) passengers. Train has \(totalPassengers) passengers now.")
+
+//train is going to pick up 20 passengers
+nextPickup = 20
+totalPassengers += nextPickup
+print("picked up \(nextPickup) passengers. Train has \(totalPassengers)passengers now")
+
+//how many passengers do we have in each car?
+
+print("there are \(totalPassengers/totalCars) people in each car. With \(totalPassengers%totalCars)people left over")
+
+
+print("it is \(totalPassengers<startPassengers) that we have less passengers now than when we started.")
+
+totalPassengers += 1
+print("we decided to pick up one more")
+
+var arrayOne = [1,2,3,4,5,6,7,8,9,10]
+
+var arrayTwo:[Int] = [1,2,3,4,5,6,7,8,9,10]
+
+arrayOne.count
+
+arrayTwo.append(2)
+
+
+
+
+
+
+
+for a in 1...100 {
+    
+   print(a)
+    
+}
+
+ 
+ */
+
+//Class example:
+
+class testClass {
+    
+    var colour1 = "blue"
+    var colour2 = "red"
+}
+
+var colourHolder = testClass()
+
+colourHolder.colour1
+colourHolder.colour2
+
+
+
+
+
+
+
+
+
+
+ 
