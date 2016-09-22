30fc72febd12ead6cbfc3ed31a4bf6c6ebcd7b96
Some-T <jamie.cropley@gmail.com>
2016-09-22 20:12:42 +0100

    Update
    Update

--- SWIFT/UdemyPlayground.playground/Contents.swift
+++ SWIFT/UdemyPlayground.playground/Contents.swift
@@ -10,3 +10,16 @@ var str = "Hello, playground"
 //Constant	
 
 let cons = "Hello, playground"
+
+
+//Function
+
+func someFunction(argument: String) {
+    print(argument)
+}
+
+
+someFunction("print from function")
+
+someFunction("print something else")
+
