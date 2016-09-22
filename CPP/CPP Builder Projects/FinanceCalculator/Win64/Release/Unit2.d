a942248958ac60d2aad8b260c8c2afdc52131f5d
Some-T <jamie.cropley@gmail.com>
2016-09-22 20:17:06 +0100

    Initial Commit Part 1
    Initial Commit Part 1

---
+++ CPP/CPP Builder Projects/FinanceCalculator/Win64/Release/Unit2.d
@@ -0,0 +1,6 @@
+.\Win64\Release\Unit2.o: Unit2.cpp \
+  C:\Users\Jamie\Documents\Embarcadero\Studio\Projects\FinanceCalculator/Unit1.h \
+  c:\program\ files\ (x86)\embarcadero\studio\18.0\include\windows\vcl\Vcl.ExtDlgs.hpp \
+  c:\program\ files\ (x86)\embarcadero\studio\18.0\include\windows\vcl\Vcl.Buttons.hpp \
+  c:\program\ files\ (x86)\embarcadero\studio\18.0\include\windows\vcl\Vcl.Consts.hpp \
+  C:\Users\Jamie\Documents\Embarcadero\Studio\Projects\FinanceCalculator/Unit2.h
